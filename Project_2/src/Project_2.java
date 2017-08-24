6f69a83a71f14863bb360f7616f3acf6e4f26540
nicolelow1209 <19lown63@millshighsch.org>
2017-08-24 09:30:38 -0700

    Hello World

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,17 @@
+//Nicole Low
+//August 23, 2017
+/*This class compares output print and printlyn
+ */
+
+public class Project_2 {
+
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
