8fffb503039dd032991a5b6be0be798ca4c3ad9e
nicolelow1209 <31107038+nicolelow1209@users.noreply.github.com>
2017-11-06 09:22:36 -0800

    Update 11/6/17

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,23 @@
+/*Nicole Low
+ * September 6, 2017
+ *This is the runner for the calculate library. It is used to text the method in Calculate
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		//System.out.println(Calculate.square(7));
+		//System.out.println(Calculate.cube(7));
+		//System.out.println(Calculate.average(7, 8.0));
+		//System.out.println(Calculate.average(8.0,9,7));
+		//System.out.println(Calculate.toDegrees(3.14));
+		//System.out.println(Calculate.toRadians(180));
+		//System.out.println(Calculate.discriminant(1,4,2));
+		//System.out.println(Calculate.toImproperFrac(3,1,2));
+		//System.out.println(Calculate.toMixe	dNum(2,4));
+		//System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+	}
+	
+	
+	
+
+} 
--- ArrayMini-Lab/src/LotsOfCopies.java
+++ ArrayMini-Lab/src/LotsOfCopies.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  * LotsOfCopies
  */ 
 
+//My try of Array Mini Lab 
 public class LotsOfCopies {
 	public static void main (String [] args) {
 		int num = 7;
@@ -17,18 +18,19 @@ public class LotsOfCopies {
 		System.out.println(strMain);
 		System.out.println(Arrays.toString(arrMain));
 		
+		//integer
 		int a = 5;
 		int b = a;
 		a = 10;
-		
 		System.out.println(b);
 		
+		//String
 		String c = "two";
 		String d = c;
 		c = "98";
-		
 		System.out.println(d);
 		
+		//Array
 		int[] e = {5,2,3,4};
 		int[] f = e;
 		e[1]=10;
@@ -44,6 +46,42 @@ public class LotsOfCopies {
 		arr[2] = 4;
 		arr[3] = 5;
 		arr[4] = 6;
-	}
+		
+	// In Class Part 1
 
+		 System.out.println("In changeMe, before the change, num (aka x):"+x);
+				x=4;
+		 System.out.println("In changeMe, after the change, num (aka x):"+x);
+		
+		 System.out.println("In changeMe, before the change, strMain (aka str):"+str);
+		 System.out.println("In changeMe, before the change, strMain (aka str):"+str);
+		 		str ="The is the new str";
+
+		 System.out.println("In changeMe, before the change, asrrMain (aka arr):"+Arrays.toString(arr));
+		 		arr[0]=12;
+		 System.out.println("In changeMe, after the change, asrrMain (aka arr):"+Arrays.toString(arr));
+	
+	// In Class Part 2
+				//a = Start
+				//b = a
+				//a == new
+				//b ==???
+
+				//integer
+				//x = 3
+				//int bint = x;
+				//x = 23; 
+				//System.out.println("b" + bint);
+
+				//String
+				//str = "hhhhh";
+				//String bstr =str;
+				//str = "iiiii";
+				//System.out.println("bstr:" + bstr);
+				
+				//Array
+				//int [] barr = arr;
+				//arr [0]=200
+				//System.out.println ("barr: + Array.tostring(basrr));
+	}
 }
